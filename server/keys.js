0651f38b1a29cc2396a9803228bb2d3df1fa9444
Jean-Christophe Trebalag <jctrebalag@gmail.com>
2020-06-23 18:33:52 +0200

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPwd: process.env.PGPWD,
+  pgPort: process.env.PGPORT,
+};
